631940b834b90261b1e967c2c703858312569cd9
Andres Forero <anfforerove@gmial.com>
2024-04-02 00:30:48 -0500

    let's start with filters

--- src/app/components/countries-details/countries-details.component.ts
+++ src/app/components/countries-details/countries-details.component.ts
@@ -1,21 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CountryDetailsShow } from '../../models/country.model';
 import { DecimalPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-countries-details',
   standalone: true,
-  imports: [NgFor, NgIf, DecimalPipe],
+  imports: [NgFor, NgIf, DecimalPipe, RouterLink],
   templateUrl: './countries-details.component.html',
   styleUrl: './countries-details.component.scss',
 })
 export class CountriesDetailsComponent {
+
+  route = inject(Router)
   _countriesService = inject(CountriesService);
   activateRout = inject(ActivatedRoute);
   showCountry!: CountryDetailsShow;
-  borders:string[] = []
+  borders:any[] = []
 
   ngOnInit() {
     this.activateRout.params.subscribe((param) => {
@@ -31,11 +33,8 @@ export class CountriesDetailsComponent {
   }
 
   organizeData(response: CountryDetailsShow) {
-    
-
     response['languages'] = Object.values(response['languages']);
     response['currencies'] = Object.values(response['currencies']);
-    console.log(response['name']['nativeName']);
     response['name']['nativeName'] = Object.values(
       response['name']['nativeName']
     );
@@ -43,10 +42,19 @@ export class CountriesDetailsComponent {
   }
 
   getBordesNames(borders: string[]){
+    this.borders = [];
     borders.forEach((border) => {
       this._countriesService.getbyCode(`alpha/${border}`).subscribe(([response]) => {
-        this.borders.push(response.name['common']) 
+        
+        this.borders.push({name: response.name['common'], code: response.cca3}) 
+        
       })
     })
+  }
+
+
+
+  onCLickBack(){
+    this.route.navigate(['/countries'])
   }
 }
